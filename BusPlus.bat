4edcaa187c562e6a77c4765e31e84c637a2baadf
LinuxLover3000 <charlieyonkura@gmail.com>
2017-10-27 23:18:52 -0400

    created bare bones

--- BusPlus.bat
+++ BusPlus.bat
@@ -1,23 +1,22 @@
 @echo off
-title BusPlus for Ultimate Driving
-mode 1000
-:startup
+title BusPlus Alpha
+:start
 cls
-echo BBBBBBBBBBBBBBBBB                                      PPPPPPPPPPPPPPPPP   lllllll                                    
-echo B::::::::::::::::B                                     P::::::::::::::::P  l:::::l                                    
-echo B::::::BBBBBB:::::B                                    P::::::PPPPPP:::::P l:::::l                                    
-echo BB:::::B     B:::::B                                   PP:::::P     P:::::Pl:::::l                                    
-echo   B::::B     B:::::Buuuuuu    uuuuuu      ssssssssss     P::::P     P:::::P l::::l uuuuuu    uuuuuu      ssssssssss   
-echo   B::::B     B:::::Bu::::u    u::::u    ss::::::::::s    P::::P     P:::::P l::::l u::::u    u::::u    ss::::::::::s  
-echo   B::::BBBBBB:::::B u::::u    u::::u  ss:::::::::::::s   P::::PPPPPP:::::P  l::::l u::::u    u::::u  ss:::::::::::::s 
-echo   B:::::::::::::BB  u::::u    u::::u  s::::::ssss:::::s  P:::::::::::::PP   l::::l u::::u    u::::u  s::::::ssss:::::s
-echo   B::::BBBBBB:::::B u::::u    u::::u   s:::::s  ssssss   P::::PPPPPPPPP     l::::l u::::u    u::::u   s:::::s  ssssss 
-echo   B::::B     B:::::Bu::::u    u::::u     s::::::s        P::::P             l::::l u::::u    u::::u     s::::::s      
-echo   B::::B     B:::::Bu::::u    u::::u        s::::::s     P::::P             l::::l u::::u    u::::u        s::::::s   
-echo   B::::B     B:::::Bu:::::uuuu:::::u  ssssss   s:::::s   P::::P             l::::l u:::::uuuu:::::u  ssssss   s:::::s 
-echo BB:::::BBBBBB::::::Bu:::::::::::::::uus:::::ssss::::::sPP::::::PP          l::::::lu:::::::::::::::uus:::::ssss::::::s
-echo B:::::::::::::::::B  u:::::::::::::::us::::::::::::::s P::::::::P          l::::::l u:::::::::::::::us::::::::::::::s 
-echo B::::::::::::::::B    uu::::::::uu:::u s:::::::::::ss  P::::::::P          l::::::l  uu::::::::uu:::u s:::::::::::ss  
-echo BBBBBBBBBBBBBBBBB       uuuuuuuu  uuuu  sssssssssss    PPPPPPPPPP          llllllll    uuuuuuuu  uuuu  sssssssssss  
-pause
-echo By LinuxLover3000
+echo BusPlus
+echo.
+echo Choose an option
+echo.
+echo 1) Start a route
+echo 2) View routes
+echo 3) Exit
+echo.
+set /p choice="Enter: "
+if %choice%==1 (
+goto setup
+)
+if %choice%==2 (
+goto view
+)
+if %choice%==3 (
+exit
+)

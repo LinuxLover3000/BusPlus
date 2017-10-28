1f28ffb8ff35c6fc45584a91d7b854b7e987d71c
LinuxLover3000 <charlieyonkura@gmail.com>
2017-10-28 15:54:55 -0400

    added most of the busplus.bat code, added all of the routes into txts.

--- BusPlus.bat
+++ BusPlus.bat
@@ -20,3 +20,276 @@ goto view
 if %choice%==3 (
 exit
 )
+:setup
+cls
+echo BusPlus
+echo.
+echo Choose your route.
+echo.
+set /p route="Enter: "
+for /f "delims=... tokens=1-12" %%a in (%route%.txt) do (
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c...%%d...%%e...%%f...%%g...%%h...%%i..%%j...%%k.
+echo.
+echo How many stops are listed?
+echo.
+set /p rtlng="Enter: "
+if %rtlng%>=12 (
+goto setup
+)
+if %rtlng%<=1 (
+goto setup
+)
+goto %rtlng%
+)
+:2
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:3
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:4
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c...%%d.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:5
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c...%%d...%%e.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:6
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c...%%d...%%e...%%f.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:7
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c...%%d...%%e...%%f...%%g.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:8
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c...%%d...%%e...%%f...%%g...%%h.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:9
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c...%%d...%%e...%%f...%%g...%%h...%%i.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:10
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c...%%d...%%e...%%f...%%g...%%h...%%i...%%j.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:11
+cls
+echo BusPlus
+echo.
+echo Route %route%
+echo %%a...%%b...%%c...%%d...%%e...%%f...%%g...%%h...%%i...%%j...%%k.
+echo.
+echo Start this route?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p sroute="Enter: "
+if %sroute%==1 (
+goto route
+)
+if %sroute%==2 (
+goto start
+)
+:route
+cls
+set 1=%%a
+set 2=%%b
+set 3=%%c
+set 4=%%d
+set 5=%%e
+set 6=%%f
+set 7=%%g
+set 8=%%h
+set 9=%%i
+set 10=%%j
+set 11=%%k
+echo BusPlus
+echo.
+echo Route %route%
+if 
+:view
+cls
+echo BusPlus
+echo.
+echo Choose a route to view.
+echo.
+echo 111
+echo 112
+echo 113
+echo 114
+echo 121
+echo 122
+echo 123
+echo 124
+echo 231
+echo 241
+echo 251
+echo 252
+echo 253
+echo 261
+echo 262
+echo 263
+echo X51
+echo X52
+echo X53
+echo X61
+echo X62
+echo X63
+echo.
+set view=0
+set /p view="Enter: "
+cls
+echo BusPlus
+echo.
+echo Viewing Route %view%
+echo.
+for /f "delims=" %%a in (%view%.txt) do (
+echo %%a.
+pause
+goto start
+)
